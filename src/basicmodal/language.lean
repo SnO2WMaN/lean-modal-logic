c3f5faef2f4ba9c0aab44ff4f700b8a49c985865
SnO2WMaN <me@sno2wman.net>
2022-11-01 08:13:22 +0000

    basicmodalのlanguageを修正

--- src/basicmodal/language.lean
+++ src/basicmodal/language.lean
@@ -3,21 +3,20 @@ universe u
 namespace basicmodal
 variables (A : Type u)
  
-inductive formula : Type u
-  | atom : A → formula
-  | top : formula
-  | neg : formula → formula
-  | imply : formula → formula → formula
+inductive Formula : Type u
+  | falso : Formula
+  | imply : Formula → Formula → Formula
+  | box : Formula → Formula
 
--- notation (name := form.bot) `⊥` :80 := form.bot
--- prefix (name := form.var) `p` :80 := form.var
--- infix (name := form.and) `∧` :79 := form.and
--- notation (name := form.impl) `→` :80 := form.impl 
--- notation (name := form.not) `¬` φ :90 := (φ → ⊥)  
--- notation (name := form.or) φ `∨` ψ :79 := (¬φ → ψ)
--- notation (name := form.iff) φ `↔` ψ :78 := ((φ → ψ) ∧ (ψ → φ))
--- notation (name := form.top) `⊤` :80 := ¬⊥
--- notation (name := form.box) `□` :80 := form.box
--- notation (name := form.dia) `◇` :80 := λ φ, ¬(□(¬φ))
+notation (name := Formula.falso) `⊥'` : 85 := Formula.falso
+
+infixr (name := Formula.imply) `→'`: 55 := Formula.imply
+notation (name := Formula.neg) `¬'` φ: 60 := φ →' ⊥' 
+notation (name := Formula.disj) φ `∨'` ψ:50 := (¬'φ →' ψ)
+notation (name := Formula.conj) φ `∧'` ψ:50 := ¬'(¬'φ ∨' ¬'ψ)
+notation (name := Formula.equiv) φ `↔'` ψ:45 := (φ →' ψ) ∧' (ψ →' φ)
+
+prefix (name := Formula.box) `□` :60 := Formula.box
+notation (name := Formula.dia) `◇` φ:60 := ¬(□(¬φ))
 
 end basicmodal
